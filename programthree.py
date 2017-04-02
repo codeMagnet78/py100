15756ee75ed503e92062adcfedf45de5ca2b44c4
codeMagnet78 <codemagnet@outlook.com>
2017-04-02 13:20:24 +0100

    Program 3

---
+++ programthree.py
@@ -0,0 +1,23 @@
+'''With a given integral number n, write a program to generate a dictionary
+that contains (i, i*i) such that is an integral number between 1 and n
+(both included). and then the program should print the dictionary.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+'''
+while True:
+    try:
+        num = int(input("Please enter a value: "))
+        result = {}
+        for x in range(num+1):
+            if x != 0:
+                result[x]=x**2
+        print(result)
+        break
+    except ValueError:
+        print("Please enter the correct value")
+        continue
+
+
+
