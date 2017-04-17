718e7fe48a6cb52fe80f330863cbdfe569798d71
Abhishek Sharma <abhishek@flashmac.home>
2017-04-17 12:41:09 +0100

    Program 13: Digit or Letters

--- program_thirteen.py
+++ program_thirteen.py
@@ -6,4 +6,14 @@ Then, the output should be:
 LETTERS 10
 DIGITS 3
 '''
-
+letters = 0
+digits = 0
+inputstring = input("Please enter any text: ")
+for c in inputstring:
+    if c.isdigit():
+        digits += 1
+    elif c.isalpha():
+        letters += 1
+    else:
+        pass
+print("LETTERS {0} and DIGITS {1}".format(letters, digits))
